473c9d14c5538f327e6816e9e40bbb1df18c0827
rahul9540627370 <138368710+rahul9540627370@users.noreply.github.com>
2025-03-17 14:34:50 +0530

    change in js file

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const menuIcon = document.querySelector('.menu-icon');
 const closeIcon = document.querySelector('.close-icon');
 const navLinks = document.querySelector('.nav-links');
-
+const innerWidth=window.innerWidth
 // Menu open karega
 menuIcon.addEventListener('click', () => {
   navLinks.classList.add('show');
@@ -14,15 +14,4 @@ closeIcon.addEventListener('click', () => {
   navLinks.classList.remove('show');
   menuIcon.style.display = 'block';
   closeIcon.style.display = 'none';
-});
-
-// Screen resize pe icons aur menu reset
-window.addEventListener('resize', () => {
-  if (window.innerWidth > 768) {
-    menuIcon.style.display = 'none';
-    closeIcon.style.display = 'none';
-  } else {
-    menuIcon.style.display = 'block';
-    closeIcon.style.display = 'none';
-  }
 });
